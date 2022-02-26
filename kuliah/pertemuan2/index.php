4814e0e56f1ff9f719f2a0788d46c0eef603cff0
Dzikry123 <dzikryhabibie3@gmail.com>
2022-02-26 13:07:43 +0700

    tugas pertemuan 2

---
+++ kuliah/pertemuan2/index.php
@@ -0,0 +1,108 @@
+<?php 
+// Sintaks php
+
+//standar output
+// echo, print
+// print_r
+//var_dumb/
+
+// echo "ada  ", 123, true;
+// print '    print,';
+// print_r('    ,print_r  ');
+// var_dump("  ,var_dump  ");
+
+
+// Penulisan sintaks php
+// 1. PHP didalam php dan sebaliiknya
+
+
+
+// Variabel dan Tipe data
+// Tidak boleh diawali oleh angka
+// kutip dua ("") berguna untuk mendeteksi variabel jika kutip satu ('') itu tidsak bisa
+
+// $nama = "derty";
+
+// echo "halo nama saya $nama";
+
+//Operator
+
+// Aritmatika
+// + - / %
+// $x = 1;
+// $y =1;
+// echo $x + $y;
+
+// Penggbaung / concatination / concat
+// .
+
+// $nama_depan = "dzikry";
+// $nama_belakanag = "habibie"; 
+
+// echo $nama_depan. " " .$nama_belakanag;
+//penulisan var tidak boleh menggunakan spasi
+
+
+// Assignment
+// +=, -=, /=, %=, .=
+
+// $x = 1;
+// $x +=9;
+// echo $x
+
+// $nama = "dzikry";
+// $nama .= " ";
+// $nama .= " habibie";
+ 
+// echo $nama;
+
+// Perbandingan 
+// <, >, <=, >=, ==, !=
+// var_dump(1 == 5 );
+
+// Identitas
+// ===, !== (tidak sama dengan)
+// var_dump(1 === "1" );
+
+// LOgika
+// &&, ||, !
+
+// $x = 30;
+// var_dump($x < 10 && $x % 2 == 0 );
+// jika menggunakan && nilai boolean keduanya harus true
+
+$x = 30;
+var_dump($x < 20 || $x % 2 == 0 );
+// jika || tidak apa2 hanya benar 1
+
+
+
+
+
+
+
+$nama = "Dzikry";
+?>
+
+
+
+
+
+<!--  <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title>contoh</title>
+ </head>
+ <body>
+ 
+    <h1>hallo <?php //echo $nama; ?></h1>
+
+    <p><?php //echo "ini paragfraf"; ?></p>
+
+ tidak apa2 jika ada lebih dari satu tag php -->
+
+   <!-- <?php //echo "<h2>hello</h2>"; ?> -->
+<!-- penulisan diatas tidak disarankan -->
+
+<!-- </body>
+ </html> -->
